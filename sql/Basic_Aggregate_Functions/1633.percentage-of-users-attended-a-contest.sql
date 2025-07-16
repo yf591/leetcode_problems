f8df66afa8ece7170d7ea68f4a6cc27afa786034
yf591 <yf591231@gmail.com>
2025-07-17 00:33:06 +0900

    Solved '1633.percentage-of-users-attended-a-contest'

---
+++ sql/Basic_Aggregate_Functions/1633.percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+SELECT
+    r.contest_id,
+    ROUND((COUNT(r.user_id) * 100 / (SELECT COUNT(*) FROM Users)), 2) AS percentage
+FROM
+    Register AS r
+GROUP BY
+    r.contest_id
+ORDER BY
+    percentage DESC, r.contest_id ASC;
