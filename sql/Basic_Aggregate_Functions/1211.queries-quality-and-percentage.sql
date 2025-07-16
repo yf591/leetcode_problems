5dfb8c0636111d8a4124a6307d6f42db0d2308dc
yf591 <yf591231@gmail.com>
2025-07-17 01:28:16 +0900

    Solved '1211.queries-quality-and-percentage'

---
+++ sql/Basic_Aggregate_Functions/1211.queries-quality-and-percentage.sql
@@ -0,0 +1,10 @@
+SELECT
+    q.query_name,
+    ROUND(AVG(q.rating / q.position), 2) AS quality,
+    ROUND(SUM(CASE WHEN q.rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM
+    Queries AS q
+WHERE
+    q.query_name IS NOT NULL
+GROUP BY
+    q.query_name;
