5e116d3a62b38b95731d5c9b9db1842653b10470
yf591 <yf591231@gmail.com>
2025-07-18 15:56:52 +0900

    Fixed answer

--- sql/Subqueries/1978.employees-whose-manager-left-the-company.sql
+++ sql/Subqueries/1978.employees-whose-manager-left-the-company.sql
@@ -1,10 +1,10 @@
 SELECT
-    e.employee_id
+    e1.employee_id
 FROM
-    Employees AS e
+    Employees AS e1
 WHERE
-    e.manager_id IS NOT NULL
-    AND e.salary < 30000
-    AND e.manager_id NOT IN(SELECT employee_id FROM Employees)
+    e1.manager_id IS NOT NULL
+    AND e1.salary < 30000
+    AND e1.manager_id NOT IN(SELECT employee_id FROM Employees AS e2)
 ORDER BY
-    e.employee_id;
+    e1.employee_id;
